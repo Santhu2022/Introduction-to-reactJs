807d73982bf0ef661c61d5aabec90fadbd0ce36f
Santhu2022 <mogilipakasantosh@gmail.com>
2023-04-19 14:46:28 +0000

    Full Code

---
+++ index.js
@@ -0,0 +1,7 @@
+const element = (
+    <div>
+        <h1 className = "greeting">Hello!</h1>
+        <p>Nice to see you here</p>
+    </div>
+)
+ReactDOM.render(element, document.getElementById('root'));
